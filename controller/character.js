74d1856d8278f7040888153806ad059ffa605ead
Mark Wallace <mwallace999@gmail.com>
2024-03-10 22:49:30 -0400

    Handle damage edge cases

--- controller/character.js
+++ controller/character.js
@@ -6,14 +6,19 @@ export const removeCondition = (char, condition) => char.conditions = char.condi
 
 export const addCondition = (char, condition) => !char.conditions.includes(condition) && char.conditions.push(condition);
 
+// *** ADD TEMP HP ***
 export const addTempHp = (id, tempHp) => {
   const activeChar = getCharacter(id);
-  let message = `${activeChar.name} gains tempHp. `;
+  let message = `${activeChar.name} gains ${tempHp} tempHp. `;
 
+  // Check if character is Dead
+  if (activeChar.conditions.includes('Dead')) {
+    message += `${activeChar.name} is Dead and cannot receive tempHp. `;
+  } 
   // Check if current tempHp is greater than new tempHp
-  if (activeChar.tempHp > tempHp) message += `${activeChar.name} already has a greater number of TempHp (${activeChar.tempHp}).`;
+  else if (activeChar.tempHp > tempHp) message += `${activeChar.name} already has a greater number of TempHp (${activeChar.tempHp}). `;
+  // Update tempHp
   else {
-    // Update tempHp
     activeChar.tempHp = Math.max(tempHp, activeChar.tempHp);
     message += `${activeChar.name} now has ${activeChar.tempHp} TempHp.`;
   }
@@ -22,6 +27,7 @@ export const addTempHp = (id, tempHp) => {
   return { message, character: activeChar }
 }
   
+// *** HEAL ***
 export const heal = (id, health) => {
   const activeChar = getCharacter(id);
   let message = `${activeChar.name} gets healed for ${health} HP. `;
@@ -30,6 +36,11 @@ export const heal = (id, health) => {
   if (activeChar.currHp === activeChar.maxHp) {
     message += `${activeChar.name} is already at MAX health (${activeChar.currHp} HP). `;
   }
+  // Check if character is Dead
+  else if (activeChar.conditions.includes('Dead')) {
+    message += `${activeChar.name} is Dead and cannot be healed. `;
+  } 
+  // Heal character
   else {
     // Check if character was unconscious
     if (activeChar.currHp === 0)  {
@@ -50,6 +61,7 @@ export const heal = (id, health) => {
   return { message, character: activeChar }
 }
 
+// *** DEAL DAMAGE ***
 export const dealDamage = (id, damage, type, isCrit) => {
   const activeChar = getCharacter(id);
   let message = `${activeChar.name} is attacked with ${damage} point(s) of ${type} damage. `;
@@ -72,7 +84,7 @@ export const dealDamage = (id, damage, type, isCrit) => {
   // Handle tempHp if applicable
   if (modDamage && activeChar.tempHp) {
     // Determine new tempHp value
-    newTempHp = Math.max(activeChar.tempHp - modDamage, 0);
+    const newTempHp = Math.max(activeChar.tempHp - modDamage, 0);
     message += `${activeChar.tempHp - newTempHp} point(s) of damage were absorbed by tempHp. ${newTempHp} tempHp remaining. `;
     // Determine remaining damage value
     modDamage = Math.max(modDamage - activeChar.tempHp, 0);
@@ -84,17 +96,51 @@ export const dealDamage = (id, damage, type, isCrit) => {
   if (modDamage) {
     message += `${activeChar.name} takes ${modDamage} point(s) of ${type} damage. `
     const newHp = activeChar.currHp - modDamage;
-    // Check for instant death i.e. remaining damage after reaching 0 > maxHp
-    if (Math.abs(newHp) > activeChar.maxHp) {
+
+    // Handle damage to a Dead character
+    if (activeChar.conditions.includes('Dead')) {
+      message += `${activeChar.name} is already Dead.`
+    }
+    // Handle massive damage/instant death
+    else if (Math.abs(newHp) > activeChar.maxHp) {
       message += `${activeChar.name} suffers massive damage and dies instantly. RIP. `
       activeChar.currHp = 0;
-      removeCondition(activeChar, "Unconscious");
-      addCondition(activeChar, "Dead");
-    } else if (newHp <= 0) {
+      activeChar.deathFails = 0;
+      activeChar.deathSaves = 0;
+      removeCondition(activeChar, 'Unconscious');
+      addCondition(activeChar, 'Dead');
+    }
+    // Handle damage to an Unconcious character
+    else if (activeChar.currHp === 0) {
+      // Critical Hit
+      if (isCrit) {
+        activeChar.deathFails += 2;
+        message += `${activeChar.name} suffers a Critical Hit while unconcious and fails 2 Death Saves. `   
+      } 
+      // Normal Hit
+      else {
+        activeChar.deathFails++;
+        message += `${activeChar.name} suffers a Hit while unconcious and fails 1 Death Save. `
+      }
+      message += `${activeChar.name} now has ${Math.min(activeChar.deathFails, 3)} failed Death Saves. `
+      // Check if Dead
+      if (activeChar.deathFails >= 3) {
+        message += `${activeChar.name} has died. RIP. `
+        activeChar.deathFails = 0;
+        activeChar.deathSaves = 0;
+        removeCondition(activeChar, 'Unconscious');
+        addCondition(activeChar, 'Dead');
+      }
+      
+    }
+    // Handle reducing character to 0 HP
+    else if (newHp <= 0) {
       message += `${activeChar.name} is reduced to 0 HP and falls unconcious. `
       activeChar.currHp = 0;
-      addCondition(activeChar, "Unconscious");
-    } else {
+      addCondition(activeChar, 'Unconscious');
+    } 
+    // Handle normal damage
+    else {
       message += `${activeChar.name} is reduced to ${newHp} HP. `
       activeChar.currHp = newHp;
     }
